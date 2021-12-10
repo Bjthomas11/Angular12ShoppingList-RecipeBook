8eab98a06241dfba7ec83572d8f2ae4fe88898ae
Brian Thomas <brianthomas@Brians-Air.attlocal.net>
2021-12-10 14:54:29 -0600

    output recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../../../models/recipe"
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Recipe 1", "Recipe Description", 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1.jpg' ),
+    new Recipe("Recipe 1", "Recipe Description", 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1.jpg' )
+  ];
 
   constructor() { }
 
