af557f240f224ebc2fb4390cd41bc5ce9195a42e
Brian Thomas <brianthomas@Brians-Air.attlocal.net>
2021-12-14 21:31:00 -0600

    added service for ingredients

---
+++ src/services/ingredients.service.ts
@@ -0,0 +1,16 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngredientsService {
+  apiURL: string = "http://localhost:3000/ingredients";
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor(private http: HttpClient) { }
+  getIngredients(){
+    return this.http.get(`${this.apiURL}`)
+  }
+}
